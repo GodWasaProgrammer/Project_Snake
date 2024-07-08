21ab185e9d3abbb497c491645a9be4800101aab6
GodWasaProgrammer <bwnils@gmail.com>
2024-07-08 17:57:04 +0200

    created initial movement logic

--- Project_Snake/Snake.cpp
+++ Project_Snake/Snake.cpp
@@ -3,5 +3,10 @@
 Snake::Snake() : direction(Right)
 {
 	// start position for snake
-
+	body.push_back(createSegment(100.f, 100.f));
+	body.push_back(createSegment(80.f, 100.f));
+	body.push_back(createSegment(60.f, 100.f));
+	
+	// movement vector
+	directionVector = sf::Vector2f(20.f, 0.f);
 }
