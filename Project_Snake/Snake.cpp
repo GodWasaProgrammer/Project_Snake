9fbbfc9417de652405533b153450fa3cc767b7d9
GodWasaProgrammer <bwnils@gmail.com>
2024-07-08 18:23:34 +0200

    fleshed out the movement of the Snake

--- Project_Snake/Snake.cpp
+++ Project_Snake/Snake.cpp
@@ -10,3 +10,74 @@ Snake::Snake() : direction(Right)
 	// movement vector
 	directionVector = sf::Vector2f(20.f, 0.f);
 }
+
+sf::RectangleShape Snake::createSegment(float x, float y)
+{
+	sf::RectangleShape segment(sf::Vector2f(20.f, 20.f));
+	segment.setFillColor(sf::Color::Green);
+	segment.setPosition(x, y);
+	return segment;
+}
+
+void Snake::move()
+{
+	for (int i = body.size() - 1; i > 0; --i)
+	{
+		body[i].setPosition(body[i - 1].getPosition());
+	}
+	body[0].move(directionVector);
+}
+
+void Snake::update()
+{
+	updateDirection();
+	move();
+}
+
+void Snake::render(sf::RenderWindow& window)
+{
+	for (const auto& segment : body)
+	{
+		window.draw(segment);
+	}
+}
+
+bool Snake::checkCollision()
+{
+	sf::Vector2f headPosition = body[0].getPosition();
+
+	// check collision
+	for (size_t i = 1; i < body.size(); ++i)
+	{
+		if (body[i].getPosition() == headPosition)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Snake::updateDirection()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && direction != Down)
+	{
+		direction = Up;
+		directionVector = sf::Vector2f(0.f, -20.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && direction != Up)
+	{
+		direction = Down;
+		directionVector = sf::Vector2f(0.f, 20.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && direction != Right)
+	{
+		direction = Left;
+		directionVector = sf::Vector2f(-20.f, 0.f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && direction != Left)
+	{
+		direction = Right;
+		directionVector = sf::Vector2f(20.f, 0.f);
+	}
+}
